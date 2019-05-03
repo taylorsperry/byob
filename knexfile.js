602bc2f72d32f6869d9c343148bf02fe8283263a
Taylor Sperry <sperry.taylor@gmail.com>
2019-05-03 09:45:57 -0600

    configure production environment

--- knexfile.js
+++ knexfile.js
@@ -13,5 +13,13 @@ module.exports = {
     },
     useNullAsDefault: true,
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true,
+  },
 
 };
